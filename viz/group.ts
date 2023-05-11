84c546f05598db151d407a3edd3a9efc6e474cc1
kylegranger <kyle.granger@chello.at>
2023-05-11 17:22:46 +0200

    style: prettier formatting

--- viz/group.ts
+++ viz/group.ts
@@ -1,15 +1,13 @@
-
 /// <reference path="../node_modules/@webgpu/types/dist/index.d.ts" />
 
 export class CGroup {
+  public vao: WebGLVertexArrayObject | null
+  public transformBuffer: WebGLBuffer | null
+  public transformData: Float32Array | null
 
-    public vao: WebGLVertexArrayObject | null;
-    public transformBuffer: WebGLBuffer | null;
-    public transformData: Float32Array | null;
-
-    public constructor() {
-        this.vao = null;
-        this.transformBuffer = null;
-        this.transformData = null;
-    }
+  public constructor() {
+    this.vao = null
+    this.transformBuffer = null
+    this.transformData = null
+  }
 }
